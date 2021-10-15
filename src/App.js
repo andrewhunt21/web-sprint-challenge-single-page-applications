e76e1c7a01c6b5c1cdf4e9bb16fc7eaf6f730980
andrewhunt21 <andrewhunt21@icloud.com>
2021-10-15 11:42:37 -0700

    setting up routes and switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import Home from './components/Home';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+
+      <nav>
+        <h1>Lambda Eats</h1>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/pizza">Pizza</Link>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+    </div>
   );
 };
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+export default function Home() {
+
+    const history = useHistory();
+
+    const routeToPizza = () => {
+        history.push("/pizza");
+    }
+
+    return (
+        <div>
+            <h3>Your favorite food, delivered while coding</h3>
+
+            <button>
+
+            </button>
+
+        </div>
+    )
+}
